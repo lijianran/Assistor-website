7f48af160105749641676968107b7ed0c54939f9
简然 李 <lijianran@outlook.com>
2020-06-03 07:24:34 +0800

    完成添加教师功能

--- flaskr/lijing.py
+++ flaskr/lijing.py
@@ -67,40 +67,17 @@ def search():
         from person join education, skill, workinfo \
         on person.person_id = education.person_id and person.person_id = skill.person_id and person.person_id = workinfo.person_id \
         where person.person_id = ?', (person_id,)).fetchall()
-    print(len(result[0]))
-    result_person = db.execute(
-        'select * from person where person_id = ?', (person_id,)).fetchall()
-    result_education = db.execute(
-        'select * from education where person_id = ?', (person_id,)).fetchall()
-    result_skill = db.execute(
-        'select * from skill where person_id = ?', (person_id,)).fetchall()
-    result_workinfo = db.execute(
-        'select * from workinfo where person_id = ?', (person_id,)).fetchall()
 
     db.commit()
+    row = result[0]
+
     item = ["person_name", "gender", "id_number", "phone", "political_status", "time_Party", "time_work", "address", "resume",
             "edu_start", "time_edu_start", "school_edu_start", "major_edu_start", "edu_end", "time_edu_end", "school_edu_end", "major_edu_end",
             "skill_title", "time_skill", "skill_unit", "skill_number",
             "time_school", "work_kind", "job_post", "time_retire"]
 
-    num = 0
-    print(result_person[0][item[0]])
-    print(len(result_person[0]))
-    for i in range(9):
-        data[item[num]] = result_person[0][item[num]]
-        num = num+1
-
-    for i in range(8):
-        data[item[num]] = result_education[0][item[num]]
-        num = num+1
-
-    for i in range(4):
-        data[item[num]] = result_skill[0][item[num]]
-        num = num+1
-
-    for i in range(4):
-        data[item[num]] = result_workinfo[0][item[num]]
-        num = num+1
+    for i in range(len(row)):
+        data[item[i]] = row[item[i]]
 
     return data
 
@@ -128,23 +105,25 @@ def importData():
                 '调入大集中学时间': 21, '用工性质': 22, '工作岗位': 23, '退休时间': 24
             }
             rowVale = table.row_values(0)
-            title_id = [0 for i in range(25)]
+            title_id = [-1 for i in range(25)]
             for i in range(0, len(rowVale)):
                 title_name = rowVale[i]
                 if title_name in dict_title:
                     title_id[dict_title[title_name]] = i
-            print(title_id)
 
             db = get_lijing_db()
             for i in range(1, table.nrows):
                 row_value = table.row_values(i)
                 insert_data = [0 for i in range(25)]
                 for j in range(0, len(title_id)):
-                    insert_data[j] = row_value[title_id[j]]
-                    if type(insert_data) != 'str':
-                        insert_data[j] = str(insert_data[j])
-                    if len(insert_data[j]) == 0:
+                    if title_id[j] == -1:
                         insert_data[j] = '暂无'
+                    else:
+                        insert_data[j] = row_value[title_id[j]]
+                        if type(insert_data) != 'str':
+                            insert_data[j] = str(insert_data[j])
+                        if len(insert_data[j]) == 0:
+                            insert_data[j] = '暂无'
 
                 sql_person = 'insert into person (person_name,gender,id_number,phone,political_status,time_Party,time_work,address,resume) values (?,?,?,?,?,?,?,?,?)'
                 db.execute(sql_person,
@@ -175,3 +154,45 @@ def importData():
             error = '请导入xlsx格式的文件'
             flash(error)
             return redirect(url_for('lijing.basicInfo'))
+
+
+@bp.route('/add_person', methods=('GET', 'POST'))
+def add_person():
+    insert_data = [0 for i in range(25)]
+    item = ['person_name', "gender", "id_number", "phone", "political_status", "time_Party", "time_work", "address", "resume",
+            "edu_start", "time_edu_start", "school_edu_start", "major_edu_start", "edu_end", "time_edu_end", "school_edu_end", "major_edu_end",
+            "skill_title", "time_skill", "skill_unit", "skill_number",
+            "time_school", "work_kind", "job_post", "time_retire"]
+
+    for i in range(0, len(item)):
+        insert_data[i] = request.args.get(item[i], '暂无', type=str)
+        if insert_data[i] == '':
+            insert_data[i] = '暂无'
+
+    db = get_lijing_db()
+    sql_person = 'insert into person (person_name,gender,id_number,phone,political_status,time_Party,time_work,address,resume) values (?,?,?,?,?,?,?,?,?)'
+    db.execute(sql_person,
+               (insert_data[0], insert_data[1], insert_data[2], insert_data[3], insert_data[4],
+                insert_data[5], insert_data[6], insert_data[7], insert_data[8]))
+    person_id = db.execute(
+        'select person_id from person where id_number=?', (insert_data[2],)).fetchone()
+
+    sql_education = 'insert into education (edu_start,time_edu_start,school_edu_start,major_edu_start,edu_end,time_edu_end,school_edu_end,major_edu_end,person_id) values (?,?,?,?,?,?,?,?,?)'
+    db.execute(sql_education,
+               (insert_data[9], insert_data[10], insert_data[11], insert_data[12], insert_data[13],
+                insert_data[14], insert_data[15], insert_data[16], person_id[0]))
+
+    sql_skill = 'insert into skill (skill_title,time_skill,skill_unit,skill_number,person_id) values (?,?,?,?,?)'
+    db.execute(sql_skill,
+               (insert_data[17], insert_data[18], insert_data[19], insert_data[20], person_id[0]))
+
+    sql_workinfo = 'insert into workinfo (time_school,work_kind,job_post,time_retire,person_id) values (?,?,?,?,?)'
+    db.execute(sql_workinfo,
+               (insert_data[21], insert_data[22],
+                insert_data[23], insert_data[24], person_id[0]))
+    db.commit()
+
+    msg = '成功添加教师'+ insert_data[0] +'的信息'
+
+    # return render_template('lijing/basicInfo.html', msgs=msg)
+    return {'msg': msg}
